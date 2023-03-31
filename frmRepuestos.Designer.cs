690267de5cf0420eb36e2da490e4c44a5fdb352c
fermoya222 <fermoya222@hotmail.com>
2023-03-30 22:36:00 -0300

    classRep

--- frmRepuestos.Designer.cs
+++ frmRepuestos.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.mrcRepuestos = new System.Windows.Forms.GroupBox();
-            this.lblCodigo = new System.Windows.Forms.Label();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.lblMarca = new System.Windows.Forms.Label();
-            this.lblPrecio = new System.Windows.Forms.Label();
-            this.lblOrigen = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.optNacional = new System.Windows.Forms.RadioButton();
             this.optImportado = new System.Windows.Forms.RadioButton();
+            this.optNacional = new System.Windows.Forms.RadioButton();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.cmdMarca = new System.Windows.Forms.ComboBox();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
             this.btnGrabar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.mrcRepuestos.SuspendLayout();
@@ -49,10 +49,10 @@
             // 
             this.mrcRepuestos.Controls.Add(this.optImportado);
             this.mrcRepuestos.Controls.Add(this.optNacional);
-            this.mrcRepuestos.Controls.Add(this.textBox3);
-            this.mrcRepuestos.Controls.Add(this.textBox2);
-            this.mrcRepuestos.Controls.Add(this.textBox1);
-            this.mrcRepuestos.Controls.Add(this.comboBox1);
+            this.mrcRepuestos.Controls.Add(this.txtPrecio);
+            this.mrcRepuestos.Controls.Add(this.txtNombre);
+            this.mrcRepuestos.Controls.Add(this.txtCodigo);
+            this.mrcRepuestos.Controls.Add(this.cmdMarca);
             this.mrcRepuestos.Controls.Add(this.lblOrigen);
             this.mrcRepuestos.Controls.Add(this.lblPrecio);
             this.mrcRepuestos.Controls.Add(this.lblMarca);
@@ -65,79 +65,16 @@
             this.mrcRepuestos.TabStop = false;
             this.mrcRepuestos.Text = "Repuestos ";
             // 
-            // lblCodigo
+            // optImportado
             // 
-            this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(26, 60);
-            this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
-            this.lblCodigo.TabIndex = 0;
-            this.lblCodigo.Text = "Codigo";
-            // 
-            // lblNombre
-            // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(26, 113);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
-            this.lblNombre.TabIndex = 1;
-            this.lblNombre.Text = "Nombre";
-            // 
-            // lblMarca
-            // 
-            this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(26, 159);
-            this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(37, 13);
-            this.lblMarca.TabIndex = 2;
-            this.lblMarca.Text = "Marca";
-            // 
-            // lblPrecio
-            // 
-            this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(26, 210);
-            this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
-            this.lblPrecio.TabIndex = 3;
-            this.lblPrecio.Text = "Precio";
-            // 
-            // lblOrigen
-            // 
-            this.lblOrigen.AutoSize = true;
-            this.lblOrigen.Location = new System.Drawing.Point(26, 266);
-            this.lblOrigen.Name = "lblOrigen";
-            this.lblOrigen.Size = new System.Drawing.Size(38, 13);
-            this.lblOrigen.TabIndex = 4;
-            this.lblOrigen.Text = "Origen";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(119, 156);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(131, 21);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(119, 57);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(131, 20);
-            this.textBox1.TabIndex = 6;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(119, 110);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(131, 20);
-            this.textBox2.TabIndex = 7;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(119, 207);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(131, 20);
-            this.textBox3.TabIndex = 8;
+            this.optImportado.AutoSize = true;
+            this.optImportado.Location = new System.Drawing.Point(206, 262);
+            this.optImportado.Name = "optImportado";
+            this.optImportado.Size = new System.Drawing.Size(72, 17);
+            this.optImportado.TabIndex = 10;
+            this.optImportado.TabStop = true;
+            this.optImportado.Text = "Importado";
+            this.optImportado.UseVisualStyleBackColor = true;
             // 
             // optNacional
             // 
@@ -150,16 +87,79 @@
             this.optNacional.Text = "Nacional";
             this.optNacional.UseVisualStyleBackColor = true;
             // 
-            // optImportado
+            // txtPrecio
             // 
-            this.optImportado.AutoSize = true;
-            this.optImportado.Location = new System.Drawing.Point(206, 262);
-            this.optImportado.Name = "optImportado";
-            this.optImportado.Size = new System.Drawing.Size(72, 17);
-            this.optImportado.TabIndex = 10;
-            this.optImportado.TabStop = true;
-            this.optImportado.Text = "Importado";
-            this.optImportado.UseVisualStyleBackColor = true;
+            this.txtPrecio.Location = new System.Drawing.Point(119, 207);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(131, 20);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(119, 110);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(131, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(119, 57);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(131, 20);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // cmdMarca
+            // 
+            this.cmdMarca.FormattingEnabled = true;
+            this.cmdMarca.Location = new System.Drawing.Point(119, 156);
+            this.cmdMarca.Name = "cmdMarca";
+            this.cmdMarca.Size = new System.Drawing.Size(131, 21);
+            this.cmdMarca.TabIndex = 5;
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(26, 266);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(38, 13);
+            this.lblOrigen.TabIndex = 4;
+            this.lblOrigen.Text = "Origen";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(26, 210);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(26, 159);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 2;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(26, 113);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(26, 60);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo";
             // 
             // btnGrabar
             // 
@@ -199,10 +199,10 @@
         private System.Windows.Forms.GroupBox mrcRepuestos;
         private System.Windows.Forms.RadioButton optImportado;
         private System.Windows.Forms.RadioButton optNacional;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ComboBox cmdMarca;
         private System.Windows.Forms.Label lblOrigen;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Label lblMarca;
